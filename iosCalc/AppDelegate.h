0e58eeb6273cedfcde8421ae9a03fe3901160515
Rodney Kahane <rodney@rodneys-mbp>
2017-12-03 17:44:30 -0500

    Initial Commit

---
+++ iosCalc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iosCalc
+//
+//  Created by Rodney Kahane on 12/3/17.
+//  Copyright © 2017 Rodney Kahane. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iosCalc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iosCalc
+//
+//  Created by Rodney Kahane on 12/3/17.
+//  Copyright © 2017 Rodney Kahane. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
